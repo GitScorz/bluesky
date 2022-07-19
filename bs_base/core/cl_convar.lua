e99874469697558a5d926f0606f15d9a539ddaea
Scorz <cristalizado6@gmail.com>
2022-07-19 19:53:30 +0100

    fix: getting convar int correctly

--- bs_base/core/cl_convar.lua
+++ bs_base/core/cl_convar.lua
@@ -1,6 +1,6 @@
 COMPONENTS.Convar = {
-    MAX_CLIENTS = { value = GetConvar('sv_maxclients', 32) },
-    LOGGING = { value = tonumber(GetConvar('log_level', 0)) },
+    MAX_CLIENTS = { value = GetConvarInt('sv_maxclients', 32) },
+    LOGGING = { value = GetConvarInt('log_level', 0) },
     RRP_VERSION = { value = GetConvar('rrp_version', "UNKNOWN") },
     VOIP_CHANNEL = { value = GetConvar('voip_channel', "NOT SET") }
 }
--- bs_base/core/sv_convar.lua
+++ bs_base/core/sv_convar.lua
@@ -9,7 +9,7 @@ Citizen.CreateThread(function()
         AUTH_DB = { key = 'mongodb_auth_database', value = GetConvar('mongodb_auth_database', 'CONVAR_DEFAULT'), stop = true },
         GAME_URL = { key = 'mongodb_game_url', value = GetConvar('mongodb_game_url', 'CONVAR_DEFAULT'), stop = true },
         GAME_DB = { key = 'mongodb_game_database', value = GetConvar('mongodb_game_database', 'CONVAR_DEFAULT'), stop = true },
-        LOGGING = { value = tonumber(GetConvar('log_level', 0)), key = 'log_level', stop = false },
+        LOGGING = { value = GetConvarInt('log_level', 0), key = 'log_level', stop = false },
         RRP_VERSION = { value = GetConvar('rrp_version', "UNKNOWN"), key = 'rrp_version', stop = false },
         VOIP_CHANNEL = { value = GetConvar('voip_channel', 'CONVAR_DEFAULT'), key = 'voip_channel', stop = false }
     }
