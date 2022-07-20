297679b128161eadb1c9906887c6d7d69311c985
Scorz <cristalizado6@gmail.com>
2022-07-21 00:02:50 +0100

    style: renamed spawn location

--- bs_characters/config.lua
+++ bs_characters/config.lua
@@ -1,6 +1,6 @@
 Config = {
     NewSpawn = {
-        label = 'LSIA',
+        label = 'Los Santos International Airport',
         location = { x = -1044.84, y = -2749.85, z = 21.36, h = 0.0 }
     }
 }
