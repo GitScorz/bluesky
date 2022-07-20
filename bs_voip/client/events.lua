15dcfb755fb965899afce10e38e157d9ee066b39
Scorz <cristalizado6@gmail.com>
2022-07-21 00:03:56 +0100

    tweak: commented logs

--- bs_voip/client/events.lua
+++ bs_voip/client/events.lua
@@ -15,9 +15,9 @@ function handleInitialState()
 end
 
 AddEventHandler('mumbleConnected', function(address, isReconnecting)
-	Logger.Info('Voip', ('Connected to mumble server with address of %s, is this a reconnect %s'):format(GetConvarInt('voice_hideEndpoints', 1) == 1 and 'HIDDEN' or address, isReconnecting))
+	-- Logger.Info('Voip', ('Connected to mumble server with address of %s, is this a reconnect %s'):format(GetConvarInt('voice_hideEndpoints', 1) == 1 and 'HIDDEN' or address, isReconnecting))
 
-	Logger.Info('Voip', 'Connect to mumble, setting targets.')
+	-- Logger.Info('Voip', 'Connect to mumble, setting targets.')
 	-- don't try to set channel instantly, we're still getting data.
 	local voiceModeData = Cfg.voiceModes[mode]
 	LocalPlayer.state:set('proximity', {
@@ -28,7 +28,7 @@ AddEventHandler('mumbleConnected', function(address, isReconnecting)
 
 	handleInitialState()
 
-	Logger.Trace('Voip', 'Finished connection logic.')
+	-- Logger.Trace('Voip', 'Finished connection logic.')
 end)
 
 AddEventHandler('mumbleDisconnected', function(address)
