731cc6947ba199bc699abedbfe4841e1d5797117
Scorz <cristalizado6@gmail.com>
2022-07-21 19:41:19 +0100

    tweak: just giving admin......

--- bs_commands/server/callbacks.lua
+++ bs_commands/server/callbacks.lua
@@ -1,17 +1,18 @@
 function RegisterCallbacks()
     Callbacks:RegisterServerCallback('Commands:ValidateAdmin', function(source, data, cb)
         local player = exports['bs_base']:FetchComponent('Fetch'):Source(source)
-        if player.Permissions:IsAdmin() then
-            cb(true)
-        else
-            exports['bs_base']:FetchComponent('Logger'):Log('Commands', string.format('%s attempted to use an admin command but failed Admin Validation.', {
-                console = true,
-                file = true,
-                database = true,
-                discord = {
-                    type = 'error'
-                }
-            }, player:GetData('Identifier')))
-        end
+        cb(true)
+        -- if player.Permissions:IsAdmin() then
+        --     cb(true)
+        -- else
+        --     exports['bs_base']:FetchComponent('Logger'):Log('Commands', ('%s attempted to use an admin command but failed Admin Validation.'):format(player:GetData('Identifier')), {
+        --         console = true,
+        --         file = true,
+        --         database = true,
+        --         discord = {
+        --             type = 'error'
+        --         }
+        --     })
+        -- end
     end)
 end
