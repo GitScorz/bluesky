68b7f844c7d10758a75484bd430a52e420618a71
Scorz <cristalizado6@gmail.com>
2022-07-24 18:08:21 +0100

    feat: ui boilerplate

--- bs_ui/fxmanifest.lua
+++ bs_ui/fxmanifest.lua
@@ -4,3 +4,15 @@ game 'gta5'
 author 'Scorz'
 description 'Bluesky user interface.'
 version '1.0.0'
+
+lua54 'yes'
+
+ui_page 'web/build/index.html'
+
+client_script "client/**/*"
+server_script "server/**/*"
+
+files {
+  'web/build/index.html',
+  'web/build/**/*'
+}
