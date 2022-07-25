5297f4dd678af44485959c02443b8d2846181056
Scorz <cristalizado6@gmail.com>
2022-07-25 19:43:10 +0100

    tweak: update heading

--- bs_characters/config.lua
+++ bs_characters/config.lua
@@ -1,6 +1,6 @@
 Config = {
     NewSpawn = {
         label = 'The Richman Hotel',
-        location = { x = -1276.91, y = 310.76, z = 65.51, h = 0.0 }
+        location = { x = -1276.91, y = 310.76, z = 65.51, h = 150.05 }
     }
 }
