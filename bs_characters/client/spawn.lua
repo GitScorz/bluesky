b7b595664f052405d5f51d66b545bbd8f7da33e3
Scorz <cristalizado6@gmail.com>
2022-07-25 20:34:37 +0100

    tweak(characters/spawn): set hud display

--- bs_characters/client/spawn.lua
+++ bs_characters/client/spawn.lua
@@ -64,7 +64,7 @@ Spawn = {
         SetEntityMaxHealth(PlayerPedId(), 200)
         SetEntityHealth(player, data.HP)
         SetPedArmour(player, data.Armor)
-        DisplayRadar(true)
+        DisplayHud(true)
         SetNuiFocus(false, false)
         
         if data.action ~= nil then
