b61c9bf95c931c581b33034691fbdac8f1027544
Scorz <cristalizado6@gmail.com>
2022-08-03 23:22:12 +0100

    tweak(ui): add fontawesome brands pack

--- bs_ui/web/src/index.tsx
+++ bs_ui/web/src/index.tsx
@@ -3,6 +3,12 @@ import './index.css';
 import App from './components/App';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, far, fab);
 
 const root = createRoot(document.getElementById('root')!);
 
