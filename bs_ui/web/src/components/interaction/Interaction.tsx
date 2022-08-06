963bc171707d6cba7729ec823d074d707c286904
Scorz <cristalizado6@gmail.com>
2022-08-06 21:44:11 +0100

    tweak(ui/interaction): filter key

--- bs_ui/web/src/components/interaction/Interaction.tsx
+++ bs_ui/web/src/components/interaction/Interaction.tsx
@@ -1,19 +1,22 @@
 import { Slide } from '@mui/material';
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useNuiEvent } from '../../hooks/useNuiEvent';
-import { isEnvBrowser } from '../../utils/misc';
+import { debugData } from '../../utils/debugData';
 import './Interaction.css';
+
+debugData<UI.Interaction.Data>([
+  {
+    action: 'hud:action:showInteraction',
+    data: {
+      action: "[E] Unlocked",
+      colorType: "success",
+    },
+  }
+])
 
 export default function Interaction() {
   const [visible, setVisible] = useState(false);
   const [interactionData, setInteractionData] = useState<UI.Interaction.Data>({ action: "", colorType: "default" });
-
-  useEffect(() => {
-    if (isEnvBrowser()) {
-      setVisible(false);
-      setInteractionData(oldData => ({ ...oldData, action: "Parking", colorType: "default" }));
-    }
-  }, []);
 
   useNuiEvent('hud:action:showInteraction', (data: UI.Interaction.Data) => {
     setVisible(true);
@@ -27,9 +30,9 @@ export default function Interaction() {
   const getColor = (type: string) => {
     switch (type) {
       case 'error':
-        return '#ff0000';
+        return '#de4a43';
       case 'success':
-        return '#8cff82';
+        return '#5bb765';
       case 'default':
         return '#1e1f24';
       default:
@@ -37,10 +40,22 @@ export default function Interaction() {
     }
   }
 
+  const filterKey = (text: string) => {
+    const a = text.indexOf("[");
+    const b = text.indexOf("]");
+    const retval = text.substring(a, b + 1);
+    return retval;
+  }
+
   return (
     <Slide direction='right' timeout={{ enter: 600, exit: 500 }} in={visible}>
       <div className='action-container' style={{ backgroundColor: getColor(interactionData.colorType) }}>
-        <div className='action-text'>{interactionData.action}</div>
+        <div className='action-text'>
+          {filterKey(interactionData.action) && (
+            <span id="action-text-key">{filterKey(interactionData.action)}</span>
+          )}
+          <span id="action-text-value">{interactionData.action.replace(filterKey(interactionData.action), '')}</span>
+        </div>
       </div>
     </Slide>
   )
