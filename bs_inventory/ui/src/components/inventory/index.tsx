6f539506f79b1b44adb927145eb76539a95f6fef
Scorz <cristalizado6@gmail.com>
2022-08-10 19:55:30 +0100

    tweak(inventory): fuck redux imma just use recoil instead

--- bs_inventory/ui/src/components/inventory/index.tsx
+++ bs_inventory/ui/src/components/inventory/index.tsx
@@ -1,10 +1,6 @@
-import { useAppDispatch, useAppSelector } from '../../hooks/dispatch';
 import './index.css';
 
 export default function Inventory() {
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
-
   return (
     <div className="inventory-wrapper">
       <div className="inventory-grid"></div>
