db8a34c3fb967da381ef76d6f61c1b5025f57ee2
Scorz <cristalizado6@gmail.com>
2022-08-22 13:59:20 +0100

    feat(assets): add minimap

---
+++ [assets]/assets_minimap/client.lua
@@ -0,0 +1,31 @@
+CreateThread(function()
+  SetMapZoomDataLevel(0, 0.96, 0.9, 0.08, 0.0, 0.0) -- Level 0
+  SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0) -- Level 1
+  SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0) -- Level 2
+  SetMapZoomDataLevel(3, 12.3, 0.9, 0.08, 0.0, 0.0) -- Level 3
+  SetMapZoomDataLevel(4, 24.3, 0.9, 0.08, 0.0, 0.0) -- Level 4
+  SetMapZoomDataLevel(5, 55.0, 0.0, 0.1, 2.0, 1.0) -- ZOOM_LEVEL_GOLF_COURSE
+  SetMapZoomDataLevel(6, 450.0, 0.0, 0.1, 1.0, 1.0) -- ZOOM_LEVEL_INTERIOR
+  SetMapZoomDataLevel(7, 4.5, 0.0, 0.0, 0.0, 0.0) -- ZOOM_LEVEL_GALLERY
+  SetMapZoomDataLevel(8, 11.0, 0.0, 0.0, 2.0, 3.0) -- ZOOM_LEVEL_GALLERY_MAXIMIZE
+end)
+
+CreateThread(function()
+  while true do
+    if InCar then
+      Wait(200)
+    else
+      Wait(5000)
+    end
+
+    InCar = false
+
+    local ped = PlayerPedId()
+    local isInCar = IsPedInAnyVehicle(ped, true)
+
+    if isInCar then
+      InCar = true
+      SetRadarZoom(1100)
+    end
+  end
+end)
